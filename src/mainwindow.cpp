a513a62f0a82376f05f620f87f6e1aab41b4a233
Lasse Liimatta <lasse.liimatta@aalto.fi>
2017-12-02 15:27:44 +0200

    Initial version

---
+++ src/mainwindow.cpp
@@ -0,0 +1,26 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <SFML/Audio.hpp>
+#include <iostream>
+
+MainWindow::MainWindow(QWidget *parent) :
+    QMainWindow(parent),
+    ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
+sf::Music music;
+void MainWindow::on_playButton_clicked()
+{
+    if (!music.openFromFile("/u/38/liimatl3/unix/Desktop/dankshit.wav"))
+        return; // error
+
+    music.play();
+}
+
